40dcebc11292f712a314c7ecf3cd47eb03656bd7
Andrei Dzebysh <presnia@yahoo.com>
2020-11-13 10:57:00 +0300

    Finished part 1

--- js/main.js
+++ js/main.js
@@ -9,3 +9,94 @@ menuToggle.addEventListener('click', function (event) {
   // вешаем класс на меню, когда кликнули по кнопке меню 
   menu.classList.toggle('visible');
 })
+
+const loginElem = document.querySelector('.login');
+const loginForm = document.querySelector('.login-form');
+const emailInput = document.querySelector('.login-email');
+const passwordInput = document.querySelector('.login-password');
+const loginSignUp = document.querySelector('.login-signup');
+
+const userElem = document.querySelector('.user');
+const userNameElem = document.querySelector('.user-name');
+
+const listUsers = [
+  {
+    id: '01',
+    email: 'debysh@rambler.ru',
+    password: '2143656',
+    displayName: 'Presnia',
+  },
+  {
+    id: '02',
+    email: 'hrundel@firefox.com',
+    password: '234567',
+    displayName: 'Hrundel',
+  },
+];
+
+
+const setUsers = {
+  user: null,
+  logIn(email, password, handler) {
+    const user = this.getUser(email);
+    if (user && user.password === password) {
+      this.authorizedUser(user);
+      handler();
+    } else {
+      alert('Пользователь с такими данными не найден')
+    }
+  },
+  logOut() {
+    console.log('logOut');
+  },
+  signUp(email, password, handler) {
+    if (!this.getUser(email)) {
+      const user = {email, password, displayName: email};
+      listUsers.push(user);
+      this.authorizedUser(user);
+      handler();
+    } else {
+      alert('Пользователь с таким email уже зарегистрирован');
+    }
+  },
+  getUser(email) {
+    return listUsers.find((item) => item.email === email)
+  },
+  authorizedUser(user) {
+    this.user = user;
+  }
+};
+
+const toggleAuthDom = () => {
+  const user = setUsers.user;
+  console.log('user: ', user);
+
+  if(user) {
+    loginElem.style.display = 'none';
+    userElem.style.display = '';
+    userNameElem.textContent = user.displayName;
+  } else {
+    loginElem.style.display = '';
+    userElem.style.display = 'none';
+  }
+};
+
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  setUsers.logIn(emailInput.value, passwordInput.value, toggleAuthDom);
+  
+});
+
+loginSignUp.addEventListener('click', event => {
+  event.preventDefault();
+
+  setUsers.signUp(emailInput.value, passwordInput.value, toggleAuthDom);
+  
+});
+
+toggleAuthDom();
+
+
+
+
